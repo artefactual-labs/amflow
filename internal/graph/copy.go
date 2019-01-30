889124f0cf42c5181866207c6decc09be63756da
Jesús García Crespo <jesus@sevein.com>
2019-01-30 08:06:33 -0800

    graph: test gcopy reuses edge values

--- internal/graph/copy.go
+++ internal/graph/copy.go
@@ -3,7 +3,7 @@ package graph
 import "gonum.org/v1/gonum/graph"
 
 // gcopy mimics graph.Copy but it uses the same edges instead of creating new
-// objects, so the properties are carried over.
+// objects so we don't lose their extra properties.
 func gcopy(dst graph.Builder, src graph.Graph) {
 	nodes := src.Nodes()
 	for nodes.Next() {
@@ -14,6 +14,10 @@ func gcopy(dst graph.Builder, src graph.Graph) {
 		u := nodes.Node()
 		to := src.From(u.ID())
 		for to.Next() {
+			// The original implementation does the following:
+			//   v := to.Node()
+			//   dst.SetEdge(dst.NewEdge(u, v))
+			// This is what we're doing instead to preserve the origina value:
 			dst.SetEdge(src.Edge(u.ID(), to.Node().ID()))
 		}
 	}
