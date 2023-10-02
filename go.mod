cb0777015b2293f98b7d808d9497992427e9ce2c
Jesús García Crespo <jesus@sevein.com>
2023-10-02 08:28:34 +0000

    Add save button
    This commit adds a Save button to the navigation bar. When clicked, it fetches
    the document again from the API server returning a Blob that is provided to the
    browser as a download. It uses FileSaver.js with provides cross-browser
    compatibility with a simplified API (`saveAs`).

--- go.mod
+++ go.mod
@@ -35,6 +35,5 @@ require (
 	github.com/zach-klippenstein/goregen v0.0.0-20160303162051-795b5e3961ea // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/tools v0.7.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- web/main.js
+++ web/main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import panzoom from 'panzoom';
+import { saveAs } from 'file-saver';
 import createClient from './client.es6.js'
 import getLocation from './location.js'
 
@@ -8,31 +9,45 @@ const client = createClient(scheme, host);
 
 const parse = function(blob) {
   const parser = new DOMParser();
-  const doc = parser.parseFromString(blob, 'text/xml');
+  const doc = parser.parseFromString(blob, "text/xml");
   return doc.documentElement;
 }
 
+const loadDocument = function(config) {
+  return client.showWorkflow("/workflow/default", config);
+}
+
 const loadGraph = function() {
-  client.showWorkflow("/workflow/default").then((resp) => {
+  loadDocument().then((resp) => {
     try {
-      const viewport = document.querySelector('#viewport');
-      viewport.innerHTML = '';
+      const viewport = document.querySelector("#viewport");
+      viewport.innerHTML = "";
       viewport.appendChild(parse(resp.data));
       panzoom(viewport, { minZoom: 0.01, zoomSpeed: 0.3, autocenter: true }).zoomAbs(0, 0, 0.1);
     } catch (error) {
       console.error(error);
-      alert('An error occurred while loading the graph.');
+      alert("An error occurred while loading the graph.");
     }
   }).catch((error) => {
     console.error(error);
-    alert('An error occurred while retrieving the data.');
+    alert("An error occurred while retrieving the data.");
   })
 };
+
+const saveGraph = function() {
+  loadDocument({ responseType: "blob" }).then((resp) => {
+    saveAs(resp.data, "workflow.svg", {type: "image/svg+xml"});
+  }).catch((error) => {
+    console.error(error);
+    alert("An error occurred while retrieving the data.");
+  })
+}
 
 document.querySelector('#app').innerHTML = `
   <div class="wrapper">
     <div class="box sidebar">
       <button class="reload">Reload</button>
+      <button class="save">Save</button>
     </div>
     <div class="box content">
       <div id="viewport"></div>
@@ -41,6 +56,8 @@ document.querySelector('#app').innerHTML = `
 `
 
 document.querySelector('.reload').addEventListener('click', loadGraph);
+
+document.querySelector('.save').addEventListener('click', saveGraph);
 
 document.body.addEventListener('click', function(event) {
   const el = event.target.closest('.node');
